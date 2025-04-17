3ae7603575c83424ed0b01e52bf7e25fa5de32ed
Yuri <21-22-66@mail.ru>
2025-04-17 11:13:15 +0300

    Client

--- client/src/entities/news/lib/newsThunk.ts
+++ client/src/entities/news/lib/newsThunk.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { SnippetNewsSchema } from '../model/schema';
 import { SnippetNews } from '../model/types';
 
-export const getNews = createAsyncThunk('posts/getPosts', async () => {
+export const getNews = createAsyncThunk('news/getNews', async () => {
   const response = await axios.get<SnippetNews[]>('/api/posts/');
 
   return SnippetNewsSchema.array().parse(response.data);
